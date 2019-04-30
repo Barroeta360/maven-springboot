cb5e7c0614bc804e90848eb197914687394dc655
Maximo Barroeta <mbarroeta1990@gmail.com>
2019-04-29 21:38:34 -0400

    start

--- src/main/java/com/mymaventutorial/app/App.java
+++ src/main/java/com/mymaventutorial/app/App.java
@@ -11,6 +11,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication // 
 public class App 
 {
+    public String home(){
+        return "Hello World";
+    }
+    
     public static void main( String[] args )
     {
         System.out.println( "Hello this work!" );
